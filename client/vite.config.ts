5130e4b8780b1069531341d95aef011d8201a3fd
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-12-03 13:17:01 +0200

    add  aliases for path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@common": path.resolve(__dirname, "./src/modules/common"),
+      "@modules": path.resolve(__dirname, "./src/modules"),
+    },
+  },
 })
