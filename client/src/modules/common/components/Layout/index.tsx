19429358f40295e08379d97dd45552642d993973
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-12-05 12:43:05 +0200

    add Layout to common

---
+++ client/src/modules/common/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import { NavigationMenu } from "@/modules/common/components/NavigationMenu";
+import { TopMenu } from "@/modules/common/components/TopMenu";
+
+export const Layout = () => {
+  return (
+    <div className="d-flex flex-column vh-100">
+      <TopMenu />
+
+      <div className="d-flex flex-grow-1">
+        <NavigationMenu />
+
+        <main className="flex-grow-1 overflow-auto p-4">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
