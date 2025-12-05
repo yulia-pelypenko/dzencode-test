9d5c29cfc52571ebe6588ebf985540348d65f3f4
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-12-05 12:27:45 +0200

    simplify useDate hook for better reusability and clarity

--- client/src/modules/common/utils/useDate.tsx
+++ client/src/modules/common/utils/useDate.tsx
@@ -1,23 +1,12 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useDate = () => {
   const [date, setDate] = useState(() => new Date());
-  const lastMinuteRef = useRef<number>(new Date().getMinutes());
 
-  useEffect(() => {
-    const update = () => {
-      const now = new Date();
-      const minute = now.getMinutes();
-
-      if (lastMinuteRef.current === minute) return; 
-
-      lastMinuteRef.current = minute;
-      setDate(now);
-    };
-
-    update(); 
-
-    const interval = setInterval(update, 1000); 
+useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000); 
 
     return () => clearInterval(interval);
   }, []);
