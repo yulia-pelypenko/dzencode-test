530a0bd5c186c4096678af6f20e8c5ec552c7dfd
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-12-05 12:42:39 +0200

    add NavigationMenu to common

---
+++ client/src/modules/common/components/NavigationMenu/index.tsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router-dom";
+import styles from "./styles.module.scss";
+import { ROUTES } from "@/modules/common/constants/routes";
+import clsx from "clsx";
+import { mockUser } from "@/mock/userMock";
+
+export const NavigationMenu = () => {
+  return (
+    <nav className={`d-flex flex-column align-items-center gap-5 ${styles.navmenu}`}>
+      <div className={`rounded-circle overflow-hidden ${styles.navmenu__user}`}>
+        <img
+          src={mockUser.avatar}
+          alt="User avatar"
+          className={styles.navmenu__avatar}
+        />
+      </div>
+      
+      <ul className="list-unstyled m-0 p-0 d-flex flex-column align-items-center gap-2">
+        <li className={`${styles.navmenu__item}`}>
+          <NavLink
+            to={ROUTES.ORDERS}
+            className={({ isActive }) =>
+              clsx(styles.navmenu__link, {
+                [styles.navmenu__link_active]: isActive,
+              })
+            }
+          >
+            Orders
+          </NavLink>
+        </li>
+
+        <li className={`${styles.navmenu__item}`}>
+          <NavLink
+            to={ROUTES.PRODUCTS}
+            className={({ isActive }) =>
+              isActive
+                ? `${styles.navmenu__link} ${styles.navmenu__link_active}`
+                : styles.navmenu__link
+            }
+          >
+            Products
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ client/src/modules/common/constants/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  PRODUCTS: "/products",
+  ORDERS: "/orders",
+} as const;
