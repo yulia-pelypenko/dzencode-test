74aeeb2754a04737847fc36a01ca661059d4b3e5
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-12-07 20:09:09 +0200

    add bolt style to title for list

--- client/src/modules/common/components/ListTitle/index.tsx
+++ client/src/modules/common/components/ListTitle/index.tsx
@@ -8,10 +8,10 @@ interface Props {
 export const ListTitle: FC<Props> = ({ title, count }) => {
   return (
     <div className="d-flex align-items-center gap-2">
-      <h2 className="h5 m-0">
+      <h2 className="h5 m-0 fw-bold">
         {title}
         {count && (
-          <span className="text-muted"> / {count}</span>
+          <span className="text-muted fw-bold"> / {count}</span>
         )}
       </h2>
     </div>
