1a74fe165a74ae9dbe3f51bacdf89d0026f9e4c4
Yulia Pelypenko <yulia.pelypenko.work@gmail.com>
2025-12-15 20:32:14 +0200

    setup cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.enableCors({
+    origin: configService.get<string>('CLIENT_URL'),
+  });
+
   const port = configService.get<number>('SERVER_PORT');
   await app.listen(port || 3000);
 }
